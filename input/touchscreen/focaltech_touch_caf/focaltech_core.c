1730204b9bd421b240c3bd642850345d15560820
duckyduckg <duckyduckg65@gmail.com>
2025-04-06 02:46:24 +0500

    input: touch: FTS: change probe name * for 4.19 dts
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- input/touchscreen/focaltech_touch_caf/focaltech_core.c
+++ input/touchscreen/focaltech_touch_caf/focaltech_core.c
@@ -1882,7 +1882,7 @@ static const struct i2c_device_id fts_ts_id[] = {
 	{},
 };
 static const struct of_device_id fts_dt_match[] = {
-    {.compatible = "focaltech,fts_ts", },
+    {.compatible = "focaltech,focal", },
     {},
 };
 MODULE_DEVICE_TABLE(of, fts_dt_match);
