2c07cfbc3d365c44f8cd3ec760c6fef62834ebb8
duckyduckg <duckyduckg65@gmail.com>
2025-07-06 10:32:09 +0500

    input: fp: goodix: remove latest changes
    Revert "input: fingerprint: goodix: fix redefined macro for dipper"
    
    This reverts commit baae91d723cc5b682f9165abb29ee7e0e57bebe0.
    
    Revert "input/fingerprint/goodix_ta: drop calls to dsi_bridge_interface_enable"
    
    This reverts commit 1fcb3e63a3c197e3b235fa4aa4a033ff4e49e39e.
    
    Revert "input/fingerprint/goodix_ta: remove GF_KEY_HOME"
    
    This reverts commit a0ebadbf94ea4399c0b11328d94f18f63fe4128d.
    
    Revert "Input: fingerprint: goodix: Import Changes from newer driver"
    
    This reverts commit 61f2fd71b5608fb725779b390e97c7554256afac.
    
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- input/fingerprint/goodix_ta/netlink.c
+++ input/fingerprint/goodix_ta/netlink.c
@@ -2,7 +2,7 @@
  * netlink interface
  *
  * Copyright (c) 2017 Goodix
- * Copyright (C) 2022 XiaoMi, Inc.
+ * Copyright (C) 2019 XiaoMi, Inc.
  */
 #include <linux/init.h>
 #include <linux/module.h>
@@ -26,40 +26,38 @@ void sendnlmsg(char *message)
 	int len = NLMSG_SPACE(MAX_MSGSIZE);
 	int slen = 0;
 	int ret = 0;
-
 	if (!message || !nl_sk || !pid) {
 		return;
 	}
-
 	skb_1 = alloc_skb(len, GFP_KERNEL);
-
 	if (!skb_1) {
 		pr_err("alloc_skb error\n");
 		return;
 	}
-
 	slen = strlen(message);
 	nlh = nlmsg_put(skb_1, 0, 0, 0, MAX_MSGSIZE, 0);
+
 	NETLINK_CB(skb_1).portid = 0;
 	NETLINK_CB(skb_1).dst_group = 0;
-	message[slen] = '\0';
-	memcpy(NLMSG_DATA(nlh), message, slen + 1);
-	ret = netlink_unicast(nl_sk, skb_1, pid, MSG_DONTWAIT);
 
+	message[slen] = '\0';
+	memcpy(NLMSG_DATA(nlh), message, slen+1);
+
+	ret = netlink_unicast(nl_sk, skb_1, pid, MSG_DONTWAIT);
 	if (!ret) {
 		/*kfree_skb(skb_1);*/
-		pr_err("send msg from kernel to usespace failed ret 0x%x\n",
-		       ret);
+		pr_err("send msg from kernel to usespace failed ret 0x%x\n", ret);
 	}
+
 }
+
 
 void nl_data_ready(struct sk_buff *__skb)
 {
 	struct sk_buff *skb;
 	struct nlmsghdr *nlh;
 	char str[100];
-	skb = skb_get(__skb);
-
+	skb = skb_get (__skb);
 	if (skb->len >= NLMSG_SPACE(0)) {
 		nlh = nlmsg_hdr(skb);
 		memcpy(str, NLMSG_DATA(nlh), sizeof(str));
@@ -68,21 +66,24 @@ void nl_data_ready(struct sk_buff *__skb)
 	}
 }
 
+
 int netlink_init(void)
 {
 	struct netlink_kernel_cfg netlink_cfg;
 	memset(&netlink_cfg, 0, sizeof(struct netlink_kernel_cfg));
+
 	netlink_cfg.groups = 0;
 	netlink_cfg.flags = 0;
 	netlink_cfg.input = nl_data_ready;
 	netlink_cfg.cb_mutex = NULL;
-	nl_sk = netlink_kernel_create(&init_net, NETLINK_TEST, &netlink_cfg);
+
+	nl_sk = netlink_kernel_create(&init_net, NETLINK_TEST,
+			&netlink_cfg);
 
 	if (!nl_sk) {
 		pr_err("create netlink socket error\n");
 		return 1;
 	}
-
 	return 0;
 }
 
@@ -92,6 +93,6 @@ void netlink_exit(void)
 		netlink_kernel_release(nl_sk);
 		nl_sk = NULL;
 	}
-
 	pr_info("self module exited\n");
 }
+
